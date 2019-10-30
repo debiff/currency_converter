3b050d3eba7d53d34bc5a71c004c258035ed2bfb
Simone Biffi <simone.biffi.91@gmail.com>
2019-10-30 14:59:07 +0100

    Refactor currencies_per_day to get value from db

--- src/managers/ecb_manager.py
+++ src/managers/ecb_manager.py
@@ -7,22 +7,13 @@ from src.models.day import Currency, Day
 
 
 def currencies_per_day(reference_date: datetime) -> Dict:
-    rates = requests.get(
-        "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml"
-    )
 
-    # The real root of the rates are the third node of the xml
-    rates_root = ET.fromstring(rates.content)[2]
+    day = Day.objects.get(reference_date=reference_date)
 
-    for cube in rates_root:
-        if datetime.strptime(cube.attrib["time"], "%Y-%m-%d") == reference_date:
-            break
+    currencies = {"EUR": float(1)}
 
-    currencies = {"EUR": 1}
-
-    for currency in cube:
-        if "currency" in currency.attrib:
-            currencies[currency.attrib["currency"]] = float(currency.attrib["rate"])
+    for currency in day.currencies:
+        currencies[currency.name] = float(currency.amount)
 
     return currencies
 
